811a195769ac131f589f4e1295f0462bfbfe308c
Carlos Fdez. Llamas <hello@sirikon.me>
2021-07-30 21:32:32 +0200

    tweaks

--- example/src/app/App.tsx
+++ example/src/app/App.tsx
@@ -1,15 +1,22 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import React, { useState } from 'react';
 
+type TextInputArgs = {
+	text: string,
+	onText: (text: string) => void
+}
+const TextInput = ({ text, onText }: TextInputArgs) =>
+	<input
+		type="text"
+		value={text}
+		onChange={(e) => onText(e.target.value)} />;
+
+
 export default () => {
 	const [text, setText] = useState('Hello World');
 
-	return (
-		<div>
-			<h1>{text}</h1>
-			<input type="text"
-				value={text}
-				onChange={(e) => setText(e.target.value)} />
-		</div>
-	);
+	return <>
+		<h1>{text}</h1>
+		<TextInput text={text} onText={setText}></TextInput>
+	</>;
 };
