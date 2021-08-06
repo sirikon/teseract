4af8214048d8d73f0ecf4b99f5edf76b1e4f25c7
Carlos Fdez. Llamas <hello@sirikon.me>
2021-08-06 17:13:19 +0200

    remove old code

--- old/src/commands/release.js
+++
@@ -1,7 +0,0 @@
-import lint from './lint.js';
-import build from './build.js';
-
-export default async function() {
-    await lint();
-    await build();
-}
--- old/src/commands/test.js
+++
@@ -1,9 +0,0 @@
-import path from 'path';
-import { spawnSync } from 'child_process';
-
-export default async function() {
-    spawnSync('mocha', ['--require', 'ts-node/register', './**/*.spec.ts'], {
-        stdio: 'inherit',
-        cwd: path.join(process.cwd(), 'test')
-    })
-}
